c443d297e00318231699ae02f669cbdd59f90a10
alenzadravec <32908267+alenzadravec@users.noreply.github.com>
2020-05-04 12:25:52 +0200

    Pokazna setana

--- Covjece/Assets/Scripts/Row.cs
+++ Covjece/Assets/Scripts/Row.cs
@@ -9,15 +9,131 @@ public class Row : MonoBehaviour
 
     public bool rowStopped;
     public string stoppedSlot;
-    
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Row[] rows;
+
+    [SerializeField]
+    private int prizeValue;
+    private bool resultsChecked = false;
+
+
+ 
+    public void govno()
     {
-        rowStopped = true;
-        SpinButton.HandlePulled += StartRotating; 
+        if (!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped)
+        {
+            prizeValue = 0;
+            resultsChecked = false;
+        }
+        if (!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped)
+        {
+            CheckResults();
+        }
+    }
+    public void CheckResults()
+    {
+        if (rows[0].stoppedSlot == "Diamond"
+            && rows[1].stoppedSlot == "Diamond"
+            && rows[2].stoppedSlot == "Diamond")
+
+        { prizeValue = 200; }
+
+        else if (rows[0].stoppedSlot == "Crown"
+            && rows[1].stoppedSlot == "Crown"
+            && rows[2].stoppedSlot == "Crown")
+
+        { prizeValue = 400; }
+
+        else if (rows[0].stoppedSlot == "Melon"
+            && rows[1].stoppedSlot == "Melon"
+            && rows[2].stoppedSlot == "Melon")
+
+        { prizeValue = 600; }
+
+        else if (rows[0].stoppedSlot == "Bar"
+            && rows[1].stoppedSlot == "Bar"
+            && rows[2].stoppedSlot == "Bar")
+
+        { prizeValue = 800; }
+
+        else if (rows[0].stoppedSlot == "Seven"
+            && rows[1].stoppedSlot == "Seven"
+            && rows[2].stoppedSlot == "Seven")
+
+        { prizeValue = 1500; }
+
+        else if (rows[0].stoppedSlot == "Cherry"
+            && rows[1].stoppedSlot == "Cherry"
+            && rows[2].stoppedSlot == "Cherry")
+
+        { prizeValue = 3000; }
+
+        else if (rows[0].stoppedSlot == "Lemon"
+            && rows[1].stoppedSlot == "Lemon"
+            && rows[2].stoppedSlot == "Lemon")
+
+        { prizeValue = 5000; }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Diamond")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Diamond")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Diamond"))
+        {
+            prizeValue = 100;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Crown")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Crown")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Crown"))
+        {
+            prizeValue = 300;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Melon")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Melon")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Melon"))
+        {
+            prizeValue = 500;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Bar")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Bar")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Bar"))
+        {
+            prizeValue = 700;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Seven")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Seven")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Seven"))
+        {
+            prizeValue = 1000;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Cherry")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Cherry")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Cherry"))
+        {
+            prizeValue = 2000;
+        }
+
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot) && (rows[0].stoppedSlot == "Lemon")
+                || (rows[0].stoppedSlot == rows[2].stoppedSlot) && (rows[0].stoppedSlot == "Lemon")
+                || (rows[1].stoppedSlot == rows[2].stoppedSlot) && (rows[1].stoppedSlot == "Lemon"))
+        {
+            prizeValue = 4000;
+        }
+
+        resultsChecked = true;
     }
 
+
+public void onClick()
+    {
+        rowStopped = true;
+        StartRotating();
+        govno();
+    }
+ 
     private void StartRotating()
     {
         stoppedSlot = "";
@@ -30,12 +146,13 @@ public class Row : MonoBehaviour
 
         for (int i = 0; i < 30; i++)
         {
-            if (transform.position.y <= -3.5f)
+            if (transform.position.y >= -3.5f)
             {
-                transform.position = new Vector3(transform.position.x, 1.75f, 0f);
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.25f, 0f);
+                transform.position = new Vector2(transform.position.x, 1.75f);
+                transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
                 yield return new WaitForSeconds(timeInterval);
             }
+            else { break ; }
         }
         randomValue = Random.Range(60, 100);
         switch (randomValue % 3)
@@ -54,8 +171,8 @@ public class Row : MonoBehaviour
         {
             if (transform.position.y <= -3.5f)
             {
-                transform.position = new Vector3(transform.position.x, 1.75f, 0f);
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.25f, 0f);
+                transform.position = new Vector2(transform.position.x, 1.75f);
+                transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
             }
             if (i > Mathf.RoundToInt(randomValue * 0.25f))
                 timeInterval = 0.05f;
@@ -88,6 +205,6 @@ public class Row : MonoBehaviour
 
     public void OnDestroy()
     {
-        SpinButton.HandlePulled -= StartRotating;
+        StopAllCoroutines();
     }
 }
