ead7fb1c97b129138ff1fb5e25ff3f47e4882d67
MarinRatkovic-mr <62936158+MarinRatkovic-mr@users.noreply.github.com>
2020-06-01 10:34:16 +0200

    Marin
    Završeni ulazni meni

---
+++ Covjece/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        /* uzima sljedecu scenu koja se mjeri po buld indeksu */
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+    public void QuitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
